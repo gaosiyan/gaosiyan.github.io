9bc06244b592c4df0bfd629af20396016dfcfc06
gaozhan <18602912875@163.com>
2025-08-09 23:09:32 +0800

    新增调试脚本

---
+++ scripts/deploy.cmd
@@ -0,0 +1,14 @@
+@echo off
+:: 保存初始目录（当前执行脚本时的目录）
+set "INIT_DIR=%cd%"
+
+:: 获取脚本所在目录（%~dp0 表示当前脚本的目录路径，含末尾反斜杠）
+set "SCRIPT_DIR=%~dp0"
+
+:: 切换到脚本所在目录
+cd /d "%SCRIPT_DIR%"
+
+set /p commit_msg=请输入提交日志:
+
+call 格式化.cmd
+git add . && git commit -m "%commit_msg%" && git push -u origin master && npm run deploy
